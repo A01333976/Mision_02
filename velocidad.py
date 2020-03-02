10f54eccbc3217e6468cf91616ce65d465094daa
A01333976 <61155066+A01333976@users.noreply.github.com>
2020-03-02 15:26:17 -0600

    DTV_A01333976

--- velocidad.py
+++ velocidad.py
@@ -1,4 +1,22 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Maximiliano Garcia,A01333976
+# Descripcion: Calculo de velocidad y tiempo con respecto a formula de distancia
 
 # Escribe tu programa después de esta línea.
+
+v1=int(input("velocidad:"))
+t1=int(input("tiempo:"))
+
+distancia1=v1*t1
+
+t2=int(input("tiempo2:"))
+
+distancia2=v1*t2
+
+d1=int(input("distancia:"))
+
+tiempo=d1/v1
+
+
+print(distancia1,'km/h', distancia2, 'km/h',tiempo,'hrs')
+
+
